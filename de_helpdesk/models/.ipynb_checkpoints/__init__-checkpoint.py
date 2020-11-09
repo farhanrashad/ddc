60bcc467b272b9420c7fd4cc876cde4c32b060af
Farhan Rashad <farhan@dynexcel.com>
2020-11-10 03:24:34 +0500

    update type to category

--- de_helpdesk/models/.ipynb_checkpoints/__init__-checkpoint.py
+++ de_helpdesk/models/.ipynb_checkpoints/__init__-checkpoint.py
@@ -2,6 +2,6 @@ from . import helpdesk_ticket
 from . import helpdesk_ticket_stage
 from . import helpdesk_ticket_tag
 from . import helpdesk_ticket_channel
-from . import helpdesk_ticket_type
+from . import helpdesk_ticket_category
 from . import helpdesk_ticket_team
 from . import res_partner 
