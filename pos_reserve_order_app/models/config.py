4f25db758a010f60f7cc34caa9e3cb6bc341afd7
Farhan Rashad <farhan@dynexcel.com>
2020-11-20 18:28:31 +0500

    update

--- pos_reserve_order_app/models/config.py
+++ pos_reserve_order_app/models/config.py
@@ -26,30 +26,3 @@ class POSConfigInherit(models.Model):
 	reserve_charge_type = fields.Selection([('percentage', "Percentage"), ('fixed', "Fixed")], string='Reservation Charge Type', default='fixed')
 	min_reserve_charges = fields.Float('Minimum amount to reserve order')
 	last_days = fields.Integer('Load Reserve Orders for Last')
-
-
-	def open_session_cb(self):
-		""" new session button
-
-		create one if none exist
-		access cash control interface if enabled or start a session
-		"""
-		self.ensure_one()
-		payment = self.env['pos.payment'].search([])
-		session = self.env['pos.session'].search([])
-
-		if not self.current_session_id:
-			if not self.company_has_template:
-				raise UserError(_("A Chart of Accounts is not yet installed in your current company. Please install a "
-								  "Chart of Accounts through the Invoicing/Accounting settings before launching a PoS session." ))
-			self._check_company_journal()
-			self._check_company_invoice_journal()
-			self._check_company_payment()
-			self._check_currencies()
-			self.env['pos.session'].create({
-				'user_id': self.env.uid,
-				'config_id': self.id
-			})
-			if self.current_session_id.state == 'opened':
-				return self.open_ui()
-		return self._open_session(self.current_session_id.id)
