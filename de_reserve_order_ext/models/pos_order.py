6af84fb138704c6777134bd1d6d738db67c09607
farhanrashad <farhan@dynexcel.com>
2020-11-22 14:22:14 +0000

    reseve order

--- de_reserve_order_ext/models/pos_order.py
+++ de_reserve_order_ext/models/pos_order.py
@@ -7,23 +7,60 @@ from odoo.exceptions import UserError, ValidationError, Warning
 class POSOrder(models.Model):
     _inherit = 'pos.order'
     
-    temp_amount = fields.Float(string='Difference Amount')
+    temp_amount = fields.Text(string='Difference Amount')
     
     def button_payment_adj_action(self):
-        vals = {}
+        vals = ovals = lvals = {}
         amount = 0
+        order_id = self.env['pos.order']
+        order_line_id = self.env['pos.order.line']
         for order in self:
             if order.state == 'reserved':
-                for payment in self.payment_ids:
-                    amount += payment.amount
-                order.temp_amount = order.amount_total - amount
-                vals = {
-                    'name': 'Reserve Order Adjustment',
-                    'amount': order.amount_total - amount,
-                    'pos_order_id': order.id,
-                    'payment_method_id': 3,
-                    'session_id': order.session_id.id,
+                ovals = {
+                    'name': order.name,
+                    'session_id': 4,
+                    'partner_id': order.partner_id.id,
+                    'date_order': order.date_order,
+                    'user_id': order.user_id.id,
+                    'delivery_date': order.delivery_date,
+                    'pos_reference': order.pos_reference,
+                    'company_id': order.company_id.id,
+                    'note': order.note,
+                    'state': 'draft',
+                    'amount_tax': order.amount_tax,
+                    'amount_total': order.amount_total,
+                    'amount_paid': order.amount_paid,
+                    'amount_return': order.amount_return,
                 }
+                order_id = self.env['pos.order'].create(ovals)
+                for line in order.lines:
+                    lvals = {
+                        'order_id': order_id.id,
+                        'product_id': line.product_id.id,
+                        'qty': line.qty,
+                        'price_unit': line.price_unit,
+                        'discount': line.discount,
+                        'price_subtotal': line.price_subtotal,
+                        'price_subtotal_incl': line.price_subtotal_incl,
+                    }
+                order_line_id = self.env['pos.order.line'].create(lvals)
+                for payment in order.payment_ids:
+                    #payment.update({
+                    #    'session_id': order_id.session_id,id,
+                    #    'pos_order_id': order_id.id,
+                    #})
+                    amount += payment.amount
+                    vals = {
+                        #'name': 'Reserve Order Adjustment',
+                        'amount': payment.amount,
+                        'pos_order_id': order_id.id,
+                        'payment_method_id': payment.payment_method_id.id,
+                        'session_id': order_id.session_id.id,
+                        'partner_id': order_id.partner_id.id,
+                    }
+                order.temp_amount = order.amount_total - amount
+                
+                
         pos_payment_id = self.env['pos.payment'].create(vals)
 
 class PosPayment(models.Model):
