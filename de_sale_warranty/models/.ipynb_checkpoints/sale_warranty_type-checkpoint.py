9024c250185eca0708dda7fa7e10302761705012
farhanrashad <farhan@dynexcel.com>
2020-11-28 08:48:30 +0000

    update sale warranty

--- de_sale_warranty/models/.ipynb_checkpoints/sale_warranty_type-checkpoint.py
+++ de_sale_warranty/models/.ipynb_checkpoints/sale_warranty_type-checkpoint.py
@@ -13,6 +13,7 @@ class SaleWarrantyType(models.Model):
     _description = 'Sale Warranty Type'
     
     name = fields.Char(string='Name', required=True)
+    code = fields.Char(string='Code', required=True, size=2)
     warranty_period = fields.Integer(string='Warranty Period', required=True)
     warranty_unit = fields.Selection([('year','Year(s)'),('month','Month(s)'),
                                       ('day','Day(s)'),],string = "Unit", default='year',track_visibility='onchange', required=True)
