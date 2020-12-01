5576addc7f7a248cf12b75b18b12352ae39145e5
Farhan Rashad <farhan@dynexcel.com>
2020-12-02 03:18:30 +0500

    add pos measurement

--- de_pos_measures_note/models/__init__.py
+++
@@ -1,5 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from . import pos_order
-from . import pos_config
-from . import pos_order_measures_note
--- de_pos_measures_note/models/pos_config.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from odoo import models, fields
-
-
-class POSConfig(models.Model):
-    _inherit = 'pos.config'
-
-    iface_orderline_order_measures_notes = fields.Boolean(string='Orderline Measurement', help='Allow measurement on Orderlines.')
