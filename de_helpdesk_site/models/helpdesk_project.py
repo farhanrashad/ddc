8af326ced120f9cc19da8d51525d108c75dddae8
farhanrashad <farhan@dynexcel.com>
2021-01-04 09:59:20 +0000

    update helpdesk site

--- de_helpdesk_site/models/helpdesk_project.py
+++ de_helpdesk_site/models/helpdesk_project.py
@@ -5,6 +5,6 @@ from odoo import models, fields, api
 class HelpdeskProjectTask(models.Model):
     _inherit = "project.task"
 
-    contact_name = fields.Char('Contact Name')
-    contact_no = fields.Char('Contact no')
+    contact_name = fields.Char(string='Contact Name', readonly=False)
+    contact_no = fields.Char(string='Contact no', readonly=False)
     
