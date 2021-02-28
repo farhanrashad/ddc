d66d8292e1d6b79a46624566f0b495b9dc913475
Farhan Rashad <farhan@dynexcel.com>
2021-02-28 12:54:27 +0500

    update helpdesk modules

--- de_helpdesk_repair_custom_views/__manifest__.py
+++ de_helpdesk_repair_custom_views/__manifest__.py
@@ -17,14 +17,14 @@
     # Check https://github.com/odoo/odoo/blob/13.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
     'category': 'Helpdesk',
-    'version': '0.1',
+    'version': '13.0.0.1',
 
     # any module necessary for this one to work correctly
     'depends': ['base','de_helpdesk_repair_mgmt','de_helpdesk_site'],
 
     # always loaded
     'data': [
-        # 'security/ir.model.access.csv',
+        'security/ir.model.access.csv',
         'views/project_task_diagnosis_views.xml',
         'views/templates.xml',
     ],
