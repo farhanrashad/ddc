4c3fdaddf510cb90e8f43353234963d699909869
dynexcel-ramzan <m.ramzan@dynexcel.co>
2021-03-08 10:20:46 +0500

    update manifest

--- to_attendance_device/models/hr_attendance.py
+++ to_attendance_device/models/hr_attendance.py
@@ -1,4 +1,7 @@
 from odoo import models, fields, api, _
+from odoo import models, fields, api, exceptions, _
+from odoo.tools import format_datetime
+
 
 
 class HrAttendance(models.Model):
@@ -41,11 +44,11 @@ class HrAttendance(models.Model):
                 ('check_in', '<=', attendance.check_in),
                 ('id', '!=', attendance.id),
             ], order='check_in desc', limit=1)
-            if last_attendance_before_check_in and last_attendance_before_check_in.check_out and last_attendance_before_check_in.check_out > attendance.check_in:
-                raise exceptions.ValidationError(_("Cannot create new attendance record for %(empl_name)s, the employee was already checked in on %(datetime)s") % {
-                    'empl_name': attendance.employee_id.name,
-                    'datetime': format_datetime(self.env, attendance.check_in, dt_format=False),
-                })
+            #if last_attendance_before_check_in and last_attendance_before_check_in.check_out and last_attendance_before_check_in.check_out > attendance.check_in:
+             #   raise exceptions.ValidationError(_("Cannot create new attendance record for %(empl_name)s, the employee was already checked in on %(datetime)s") % {
+              #      'empl_name': attendance.employee_id.name,
+               #     'datetime': format_datetime(self.env, attendance.check_in, dt_format=False),
+               # })
 
             if not attendance.check_out:
                 # if our attendance is "open" (no check_out), we verify there is no other "open" attendance
